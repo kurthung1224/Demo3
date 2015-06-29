81627e83e4830fd73fae344d151367f281004b47
kurthung1224 <kurthung1224@gmail.com>
2015-06-29 13:35:59 +0800

    Demo3

--- kdd_libsvm.scala
+++ kdd_libsvm.scala
@@ -4,9 +4,9 @@ bin/spark-shell --jars "/tmp/kdd/spark-liblinear-1.95.jar"
 import org.apache.spark.rdd.RDD
 import tw.edu.ntu.csie.liblinear._
 
-val data = Utils.loadLibSVMData(sc, "hdfs://master01:9000/kdd_output/part-*")
+val SVM_data = Utils.loadLibSVMData(sc, "hdfs://master01:9000/kdd_output/part-*")
 
-val model = SparkLiblinear.train(data, "-s 0 -c 1.0")
+val model = SparkLiblinear.train(SVM_data, "-s 0 -c 1.0")
 
 def PredictAndRecall(dataPoint:RDD[DataPoint] ,trainedModel: LiblinearModel, labelsNum:Double): (Double, Double) = { 
   val tp = dataPoint.filter{point => (point.y == labelsNum) && (point.y == trainedModel.predict(point))}.count.toDouble
@@ -21,7 +21,7 @@ def PredictAndRecall(dataPoint:RDD[DataPoint] ,trainedModel: LiblinearModel, lab
 //Log result
 val x = new Array[(Double, Double)](model.label.size)
 for(i <- 0 until model.label.size)
-  x(i) = PredictAndRecall(data, model , model.label(i))
+  x(i) = PredictAndRecall(SVM_data, model , model.label(i))
 val result = x.zipWithIndex
 
 val signal_swap = signal.map(_.swap)
--- kdd_preprocess_FS_SVD.scala
+++ kdd_preprocess_FS_SVD.scala
@@ -1,9 +1,5 @@
 import org.apache.spark.rdd.RDD
-
 val rawData = sc.textFile("/kdd/kddcup.data_10_percent_corrected")
-
-val data = rawData.map(_.split(','))
-
 
 
 //one-hot encoding
@@ -23,12 +19,12 @@ def one_hot_encoding(word:Array[String]): Map[String, Array[String]] = {
 }
 
 
-
 //obtain mapping values of "protocol"
 val protocol_temp = rawData.map{ line=>
   val buffer = line.split(',').toBuffer
   buffer.remove(2,buffer.length-2)
   val key = buffer.remove(1)
+  buffer.clear
   (key)
 }
 val protocol_temp_keys = protocol_temp.countByValue.keys.toArray
@@ -40,6 +36,7 @@ val service_temp = rawData.map{ line=>
   val buffer = line.split(',').toBuffer
   buffer.remove(3,buffer.length-3)
   val key = buffer.remove(2)
+  buffer.clear
   (key)
 }
 val service_temp_keys = service_temp.countByValue.keys.toArray
@@ -51,6 +48,7 @@ val flag_temp = rawData.map{ line=>
   val buffer = line.split(',').toBuffer
   buffer.remove(4,buffer.length-4)
   val key = buffer.remove(3)
+  buffer.clear
   (key)
 }
 val flag_keys_temp = flag_temp.countByValue.keys.toArray
@@ -67,11 +65,10 @@ val signal_keys_temp = signal_temp.countByValue.keys.toArray
 val signal = signal_keys_temp.zipWithIndex.toMap
 
 
-
 import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.regression._
 
-//Create the label points
+
 val protocol_location = 1
 val protocol_insert_location = protocol_location + 1
 val protocol_size = protocol.keys.size
@@ -86,7 +83,8 @@ val flag_location = service_location + service_size
 val flag_insert_location = flag_location + 1
 
 
-val labelsAndData = rawData.map { line =>
+
+val rawVector = rawData.map { line =>
   val buffer = line.split(',').toBuffer
   
   buffer.insertAll(protocol_insert_location, protocol(buffer(protocol_location)))
@@ -98,63 +96,116 @@ val labelsAndData = rawData.map { line =>
   buffer.insertAll(flag_insert_location, flag(buffer(flag_location)))
   buffer.remove(flag_location,1) 
 
-  val vector = Vectors.dense(buffer.init.map(_.toDouble).toArray)
+  val vector = buffer.init.map(_.toDouble).toArray
+  buffer.clear
+  (vector)
+}
 
+
+//feature scaling -- start
+import scala.collection.mutable.ArrayBuffer
+
+def transpose_Array(k:Array[Array[Double]]): Array[Array[Double]] = {
+  val row_size = k.size  //2
+  val col_size = k(0).length  //3
+  var zz = ArrayBuffer[Double]()
+  var ll = ArrayBuffer[Array[Double]]()
+  for(j <- 0 until col_size){
+    for(i <- 0 until row_size){
+      zz += k(i)(j)
+    }
+	val zz_Array = zz.toArray
+	zz.clear
+	ll += zz_Array
+  }
+  val jj = ll.toArray
+  ll.clear
+  (jj)
+}
+
+
+val rawVector_Array = rawVector.take(rawVector.count.toInt) //This array would occupy numerous mem space
+val trans_rawVector_Array = transpose_Array(rawVector_Array)
+
+
+def minMax(a: Array[Double]) : (Double, Double) = {
+  if (a.isEmpty) throw new java.lang.UnsupportedOperationException("array is empty")
+    a.foldLeft((a(0), a(0)))
+  { case ((min, max), e) => (math.min(min, e), math.max(max, e))}
+}
+
+def FS(a:Double, min:Double, max:Double) : (Double) = {
+  if(min == max){ //prevent NaN
+    (a)
+  }else{
+    val frac = max - min
+    val div = a - min
+    val new_v = (div / frac)
+    (new_v)
+  }
+}
+
+val features_num = trans_rawVector_Array.size
+var tmp = ArrayBuffer[(Double, Double)]()
+for(i <- 0 until features_num){
+  tmp += minMax(trans_rawVector_Array(i))
+}
+val compared_array = tmp.toArray
+tmp.clear
+
+
+val data = rawData.map { line =>
+  val buffer = line.split(',').toBuffer
+  
+  buffer.insertAll(protocol_insert_location, protocol(buffer(protocol_location)))
+  buffer.remove(protocol_location,1)
+
+  buffer.insertAll(service_insert_location, service(buffer(service_location)))
+  buffer.remove(service_location,1)
+
+  buffer.insertAll(flag_insert_location, flag(buffer(flag_location)))
+  buffer.remove(flag_location,1) 
+
+  for(i <- 0 until features_num){
+	val tmp_num = FS(buffer(i).toDouble, compared_array(i)._1, compared_array(i)._2)
+	buffer.insert(i+1, tmp_num.toString)
+	buffer.remove(i,1) 
+  }
+  
+  val vector = Vectors.dense(buffer.init.map(_.toDouble).toArray)
+  
   buffer.remove(0, buffer.length - 1)
   buffer.insert(1, signal(buffer(0)).toString)
   buffer.remove(0)
 
   val label = buffer(0).toDouble
-
+  
   (label, vector)
 }
+//feature scaling -- end
 
-//Normalize data ==>
-val labelsAndData_values = labelsAndData.values
 
-val dataAsArray = labelsAndData_values.map(_.toArray)
-val numCols = dataAsArray.first().length
-val n = dataAsArray.count()
-  
-val sums = dataAsArray.reduce(
-  (a,b) => a.zip(b).map(t => t._1 + t._2)
-)
-
-val sumSquares = dataAsArray.fold(
-  new Array[Double](numCols)
-)(
-  (a,b) => a.zip(b).map(t => t._1 + t._2 * t._2)
-)
-
-val stdevs = sumSquares.zip(sums).map {
-  case(sumSq,sum) => math.sqrt(n*sumSq - sum*sum)/n
+//Create a label point
+val labelsAndData = data.map{point =>
+  LabeledPoint(point._1, point._2)
 }
 
-val means = sums.map(_ / n)
+//save data to HDFS
+import org.apache.spark.mllib.util._
+MLUtils.saveAsLibSVMFile(labelsAndData, "hdfs://master01:9000/kdd_output/FS_LablePoint")
 
-def normalize(datum: Vector) = {
-  val normalizedArray = (datum.toArray, means, stdevs).zipped.map(
-    (value, mean, stdev) =>
-      if (stdev <= 0) (value - mean) else (value - mean) / stdev
-  )
-  Vectors.dense(normalizedArray)
-}
-//Normalize data <==
-
-val norm_values = labelsAndData_values.map(normalize(_))
 
 //SVD ==>
 import org.apache.spark.mllib.linalg.Matrix
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.mllib.linalg.SingularValueDecomposition
 
-val mat = new RowMatrix(norm_values)
+val mat = new RowMatrix(data.values)
 
-//dimension reduction to 2-diam
-val svd = mat.computeSVD(2, computeU = true)
+//dimension reduction to 3-dim or 2-dim
+val svd = mat.computeSVD(3, computeU = true)
 val U = svd.U
 val s = svd.s
-val V = svd.V
 
 //Vector to diag matrix
 val diag_s = Matrices.diag(s)
@@ -163,14 +214,5 @@ val diag_s = Matrices.diag(s)
 val reduce_matrix = U.multiply(diag_s)
 
 //save the matrix
-reduce_matrix.rows.saveAsTextFile("/kdd/R2")
+reduce_matrix.rows.saveAsTextFile("/kdd/3_dim")
 //SVD <==
-
-//Create a label point
-val z = labelsAndData.map{point =>
-  LabeledPoint(point._1, normalize(point._2))
-}
-
-//save data to HDFS
-import org.apache.spark.mllib.util._
-MLUtils.saveAsLibSVMFile(z, "hdfs://master01:9000/kdd/norm_data")
